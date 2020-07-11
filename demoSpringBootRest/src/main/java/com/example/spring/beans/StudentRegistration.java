7d98e5e02650468a50d4667b58537700bff92330
anjon <anjon@Bernabeu>
2020-07-11 18:22:13 +0530

    Changes updated

--- demoSpringBootRest/src/main/java/com/example/spring/beans/StudentRegistration.java
+++ demoSpringBootRest/src/main/java/com/example/spring/beans/StudentRegistration.java
@@ -10,5 +10,31 @@ public class StudentRegistration {
 	private StudentRegistration() {
 		studentRecords = new ArrayList<Student>();
 	}
+	public static StudentRegistration getInstance() {
+		if (stdregd == null) {
+			stdregd = new StudentRegistration();
+			return stdregd;
+		}
+		else {
+			return stdregd;
+		}
+		}
+	public void add(Student student) {
+		studentRecords.add(student);
+	}
+	public String updateStudent(Student student) {
+		for(int i=0; i<studentRecords.size(); i++) {
+			Student stdn =  studentRecords.get(i);
+			if(stdn.getRegNo().equals(student.getRegNo())) {
+				studentRecords.set(i, student);
+				return "updated Successfully";
+			}
+			
+		}
+		return "update un-Successfull";
+	}
+	public List<Student> getStudentRecords(){
+		return studentRecords;
+	}
+	}
 
-}
