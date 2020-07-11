3b3f36c45325ab3726a9c05e4acd01956e29f1bd
anjon <anjon@Bernabeu>
2020-07-11 16:34:11 +0530

    StudentRegistration class commit

--- demoSpringBootRest/src/main/java/com/example/spring/beans/StudentRegistration.java
+++ demoSpringBootRest/src/main/java/com/example/spring/beans/StudentRegistration.java
@@ -1,8 +1,14 @@
 package com.example.spring.beans;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentRegistration {
 	private List<Student> studentRecords;
+	private static StudentRegistration stdregd = null;  
+	
+	private StudentRegistration() {
+		studentRecords = new ArrayList<Student>();
+	}
 
 }
