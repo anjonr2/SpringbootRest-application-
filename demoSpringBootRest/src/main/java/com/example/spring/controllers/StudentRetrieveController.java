5e524fdbb86a8fcceb577843eec70174329b8e6b
anjon <anjon@Bernabeu>
2020-07-12 13:59:27 +0530

    pom.xml updated and Controller created

---
+++ demoSpringBootRest/src/main/java/com/example/spring/controllers/StudentRetrieveController.java
@@ -0,0 +1,15 @@
+package com.example.spring.controllers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Request
+
+import com.example.spring.beans.*;
+
+@Controller
+public class StudentRetrieveController {
+	
+	@Re
+
+}
