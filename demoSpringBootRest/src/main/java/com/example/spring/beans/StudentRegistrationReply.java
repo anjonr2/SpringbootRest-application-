0526e4d287f4ffd4afd5641f7d7fbb331ffa7209
anjon <anjon@Bernabeu>
2020-07-12 13:24:24 +0530

    StudentRegistrationReply class created

---
+++ demoSpringBootRest/src/main/java/com/example/spring/beans/StudentRegistrationReply.java
@@ -0,0 +1,34 @@
+package com.example.spring.beans;
+
+public class StudentRegistrationReply {
+	String name;
+	int age;
+	String regNo;
+	String regStatus;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getRegNo() {
+		return regNo;
+	}
+	public void setRegNo(String regNo) {
+		this.regNo = regNo;
+	}
+	public String getRegStatus() {
+		return regStatus;
+	}
+	public void setRegStatus(String regStatus) {
+		this.regStatus = regStatus;
+	}
+	
+
+}
