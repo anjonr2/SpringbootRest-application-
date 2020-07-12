2a0b877ad0047ba1f26f1109709cda33ed9964a5
anjon <anjon@Bernabeu>
2020-07-12 14:12:20 +0530

    Controller Updated

--- demoSpringBootRest/src/main/java/com/example/spring/controllers/StudentRetrieveController.java
+++ demoSpringBootRest/src/main/java/com/example/spring/controllers/StudentRetrieveController.java
@@ -2,14 +2,23 @@ package com.example.spring.controllers;
 
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Request
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.spring.beans.*;
 
 @Controller
 public class StudentRetrieveController {
 	
-	@Re
+	@RequestMapping(method = RequestMethod.GET, value="/student/allstudent")
+	
+	@ResponseBody
+	public List<Student> getAllStudent(){
+		return StudentRegistration.getInstance().getStudentRecords();
+	}
 
 }
